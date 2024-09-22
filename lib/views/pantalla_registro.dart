7d2bea2d840ae0029dc5ca676c3c73a532165134
Valentina Rozo <valerozobernal@gmail.com>
2024-09-22 11:09:14 -0500

    Fondo Pantalla

--- lib/views/pantalla_registro.dart
+++ lib/views/pantalla_registro.dart
@@ -13,88 +13,94 @@ class PantallaRegistro extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                'assets/reloj_SincronIA.png',
-                height: size.height * 0.2,
-              ),
+      body: Stack(
+        children: [
+          
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.asset(
+              'assets/reloj_SincronIA.png',
+              height: size.height * 0.2,
+              fit: BoxFit.contain,
             ),
-            SizedBox(height: size.height * 0.02),
-            const Text(
-              'Registro',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w500,
-                color: Color(0xFF0D47A1),
-                fontFamily: 'Roboto',
-              ),
+          ),
+          
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Image.asset(
+              'assets/bottom_SincronIA.png',
+              height: size.height * 0.2,
+              fit: BoxFit.contain,
             ),
-            const SizedBox(height: 20),
-            const Padding(padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(
-                children: [
-                  CustomTextField(
-                    labelText: 'Nombre',
-                    hintText: 'Digita tu Nombre',
-                  ),
-                  SizedBox(height: 25),
-                  CustomTextField(
-                    labelText: 'Apellido',
-                    hintText: 'Digita tu Apellido',
-                  ),
-                  SizedBox(height: 25),
-                  CustomTextField(
-                    labelText: 'E-mail',
-                    hintText: 'Digita tu correo electrónico',
-                  ),
-                  SizedBox(height: 25),
-                  CustomPasswordField(
-                    labelText: 'Contraseña',
-                    hintText: 'Digita tu contraseña',
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              text: 'REGISTRARME',
-              width: 250,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PantallaAlarma()),
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            CustomButton2(
-              text: 'Ya tengo una cuenta',
-              onPressed: () {
-              },
-              width: 250,
-            ),
-            Stack(
-              children: [
-                Align(
-                  alignment: Alignment.bottomRight,
-                  
-                    child: Image.asset(
-                      'assets/bottom_SincronIA.png',
-                      height: size.height * 0.2,
+          ),
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: size.height * 0.2),
+                    const Text(
+                      'Registro',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF0D47A1),
+                        fontFamily: 'Roboto',
+                      ),
                     ),
-                
+                    const SizedBox(height: 20),
+                    
+                    
+                    const CustomTextField(
+                      labelText: 'Nombre',
+                      hintText: 'Digita tu Nombre',
+                    ),
+                    const SizedBox(height: 20),
+                    const CustomTextField(
+                      labelText: 'Apellido',
+                      hintText: 'Digita tu Apellido',
+                    ),
+                    const SizedBox(height: 20),
+                    const CustomTextField(
+                      labelText: 'E-mail',
+                      hintText: 'Digita tu correo electrónico',
+                    ),
+                    const SizedBox(height: 20),
+                    const CustomPasswordField(
+                      labelText: 'Contraseña',
+                      hintText: 'Digita tu contraseña',
+                    ),
+                    const SizedBox(height: 40),
+
+                    // Botón de "REGISTRARME"
+                    CustomButton(
+                      text: 'REGISTRARME',
+                      width: 250,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const PantallaAlarma()),
+                        );
+                      },
+                    ),
+                   
+
+                    // Botón de "Ya tengo una cuenta"
+                    const SizedBox(height: 20),
+                    CustomButton2(
+                      text: 'Ya tengo una cuenta',
+                      onPressed: () {
+                      },
+                      width: 250,
+                    ),
+                  ],
                 ),
-              ],
-            )
-          ],
-        ),
-      ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
